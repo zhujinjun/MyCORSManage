cd4daab4802abff0a9f01ffc3ea33b71b1c7519b
World Hello <wangyizhujp@126.com>
2018-04-07 22:49:03 +0800

    添加项目文件。

---
+++ MyCORSManage/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace MyCORSManage.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ MyCORSManage/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MyCORSManage.WebApiApplication" Language="C#" %>
